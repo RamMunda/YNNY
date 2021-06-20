b5e4d0c74c8dfd492b29ff1573989a3cfe5d51c5
Ram <Ramkumar2k21@gmail.com>
2021-06-20 11:53:28 +0530

    added layout content

--- js/script.js
+++ js/script.js
@@ -41,11 +41,25 @@ var selectedDiv = document.querySelector('.overlay_layout');
 var selectedId = document.getElementById('overlay_layout');
 selectedButton.addEventListener('click',function(){
     console.log('clicked......');
-    selectedDiv.classList.add('addedclass');
+	selectedId.style.transform = 'translateX(0%)';
+    // selectedDiv.classList.add('addedclass');
 })
 
 selectedId.addEventListener('click',function(){
     console.log('clicked......');
-    selectedDiv.classList.remove('addedclass');
+	selectedId.style.transform = 'translateX(-100%)';
+    // selectedDiv.classList.remove('addedclass');
 })
-
+var cursorBoundary = document.getElementById('cursor_boundary');
+document.addEventListener('mousemove',function(e){
+	// console.log(e);
+	// cursorBoundary.style.top = `${e.pageY-30}px`;
+	// cursorBoundary.style.left = `${e.pageX-30}px`;
+})
+var audiosectiom = document.querySelector('audio');
+document.querySelector('.on').addEventListener('click',function(){
+   audiosectiom.play();
+})
+document.querySelector('.off').addEventListener('click',function(){
+	audiosectiom.pause();
+ })
